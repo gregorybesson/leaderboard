be7b36b5831c480618863bee9dfa48e1c756d675
fabrice labbe <fabrice.labbe@adfab.fr>
2013-07-18 13:53:59 +0200

    cleaning useless output

--- www/start_node.php
+++ www/start_node.php
@@ -1,11 +1,9 @@
 <?php
 if(isset($_POST['command'])){
     if($_POST['command'] === "start"){
-        echo 'start server';
         echo shell_exec('bash nodejs.sh start');
     }
     else if($_POST['command'] === "stop"){
-        echo 'stop server';
         echo shell_exec('bash nodejs.sh stop');
     }
 }
