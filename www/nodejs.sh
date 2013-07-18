0d56dd36ca01866aff3f332b70cb0b67d3ed90cc
fabrice labbe <fabrice.labbe@adfab.fr>
2013-07-18 13:32:29 +0200

    with path as it's global

--- www/nodejs.sh
+++ www/nodejs.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-NODE=/nodejs/node-v0.10.5/node
+NODE=/nodejs/node-v0.10.5/
 SERVER_JS_FILE=server.js
 USER=root
 OUT=nodejs.log
@@ -9,7 +9,7 @@ case "$1" in
 
 start)
     echo "starting node: $NODE $SERVER_JS_FILE"
-    sudo -u $USER $NODE $SERVER_JS_FILE > $OUT 2>$OUT &
+    sudo -u $USER $SERVER_JS_FILE > $OUT 2>$OUT &
     ;;
 
 stop)
