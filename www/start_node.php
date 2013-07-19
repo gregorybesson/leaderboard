566a3ee226034b8182598888541a1929b57f9361
fabrice labbe <fabrice.labbe@adfab.fr>
2013-07-19 15:05:44 +0200

    revert

--- www/start_node.php
+++ www/start_node.php
@@ -2,14 +2,11 @@
 if(isset($_POST['command'])){
     if($_POST['command'] === "start"){
         $output = shell_exec('bash nodejs.sh start');
-        echo "<pre>$output</pre>";
-        $output = shell_exec('ls');
-        echo '-------------------------\n';
-        echo "<pre>$output</pre>";
+        echo $output;
     }
     else if($_POST['command'] === "stop"){
         $output = shell_exec('bash nodejs.sh stop');
-        echo "<pre>$output</pre>";
+        echo $output;
     }
 }
 
