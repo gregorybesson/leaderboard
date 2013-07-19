6e3ba218574c20c3c57b7c3cba043e4a6b68aee6
fabrice labbe <fabrice.labbe@adfab.fr>
2013-07-19 15:04:40 +0200

    test bash exec

--- www/start_node.php
+++ www/start_node.php
@@ -1,10 +1,15 @@
 <?php
 if(isset($_POST['command'])){
     if($_POST['command'] === "start"){
-        echo shell_exec('bash nodejs.sh start');
+        $output = shell_exec('bash nodejs.sh start');
+        echo "<pre>$output</pre>";
+        $output = shell_exec('ls');
+        echo '-------------------------\n';
+        echo "<pre>$output</pre>";
     }
     else if($_POST['command'] === "stop"){
-        echo shell_exec('bash nodejs.sh stop');
+        $output = shell_exec('bash nodejs.sh stop');
+        echo "<pre>$output</pre>";
     }
 }
 
