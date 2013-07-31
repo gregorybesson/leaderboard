c8abba4bcf02a818f423602ad3b0b27a449425a9
wonknu <fabrice.labbe@adfab.fr>
2013-07-31 13:21:26 +0200

    added boolean ouput for succes / failure

--- php/save-json.php
+++ php/save-json.php
@@ -1,8 +1,16 @@
 <?php
 $file = "action.json";
-if(isset($_POST) && isset($_POST['data']))
-	$json = $_POST['data'];
+
+if(!isset($_POST) && !isset($_POST['data'])) {
+	echo 0;
+	die();
+}
+
+$json = $_POST['data'];
 
 file_put_contents($file, $json);
 
+echo 1;
+
+die();
 ?>
